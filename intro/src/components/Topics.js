c8c214d00550b4a4ee63050da8f25bb487d18328
Tom Hammer <tjhammer845@gmail.com>
2021-02-11 22:07:17 -0500

    finished

--- intro/src/components/Topics.js
+++ intro/src/components/Topics.js
@@ -1,5 +1,37 @@
 import React, { Component } from 'react';
+import {
+    Link,
+    Route
+} from 'react-router-dom'
 
-export default function Topics() {
-    return <div>TOPICS</div>
+function Topic({ match }) {
+    return <h3>{match.params.topicId}</h3>
+}
+
+export default function Topics({ match }) {
+
+    return (
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                <li>
+                    <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/components`}>Components</Link>
+                </li>
+                <li>
+                    <Link to={`${match.url}/props-v-state`}>Props vs State</Link>
+                </li>
+
+            </ul>
+
+            <hr />
+
+            <Route path={`${match.path}/:topicId`} component={Topic} />
+            <Route exact path={match.path} render={() => {
+                return <h3>Please select a topic</h3>
+            }} />
+        </div>
+    )
 }
